250931d64e5cfb189bd8445c01041d2eee4e0b41
tekihei2317 <tekihei2317@gmail.com>
2022-10-28 11:14:29 +0900

    フォロー

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -17,7 +17,7 @@ async function followerIdList(user: User) {
 }
 
 async function main() {
-  // ユーザーと、そのユーザーの記事の一覧を取得する
+  // 1. ユーザーと、そのユーザーの記事の一覧を取得する
   const userWithPosts = await prisma.user.findUnique({
     where: { username: "tekihei2317" },
     include: { articles: true },
@@ -32,13 +32,13 @@ async function main() {
 
   printObject({ userWithPosts2 });
 
-  // ある記事のコメント一覧を取得する
+  // 2. ある記事のコメント一覧を取得する
   const comments = await prisma.comment.findMany({
     where: { userId: 1 },
   });
   printObject({ comments });
 
-  // 記事を更新する
+  // 3. 記事を更新する
   const updatedArticle = await prisma.article.update({
     where: { id: 1 },
     data: { title: "Prismaが難しい" },
@@ -58,17 +58,37 @@ async function main() {
     }
   }
 
-  // フォローしているユーザー一覧を取得する
+  // 4. フォローしているユーザー一覧を取得する
   const user = await prisma.user.findFirstOrThrow();
   const followingUsers = await prisma.user.findMany({
     where: {
       id: { in: await followerIdList(user) },
     },
   });
-
   printObject(followingUsers);
 
-  // 記事とその記事のタグ一覧を取得する
+  // リレーションを使って取得する場合
+  const user2 = await prisma.user.findFirstOrThrow({
+    include: {
+      followingUsers: {
+        include: { followee: true },
+      },
+    },
+  });
+  printObject(user2.followingUsers);
+
+  // 5. フォロワー（自分をフォローしているユーザー）を取得する
+  const user3 = await prisma.user.findUniqueOrThrow({
+    where: { id: 2 },
+    include: {
+      followers: {
+        include: { follower: true },
+      },
+    },
+  });
+  printObject(user3.followers);
+
+  // 6. 記事とその記事のタグ一覧を取得する
   const articleAndtag = await prisma.article.findUnique({
     where: { id: 1 },
     include: { tags: true },
